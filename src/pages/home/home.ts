59a985d89ada56d92d9504694899fd6adfda826f
Alish Dhamala <dhamalaalish@gmail.com>
2018-04-09 14:22:51 -0500

    Flash Light
    The is my first app with Ionic.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
+import {Platform} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isOn: boolean;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private flashlight: Flashlight, private platform: Platform) {
+    this.platform.ready(). then(() => {
+      this.updateFlashlightStatus();
+    });
   }
 
+  toggle() {
+    this.flashlight.toggle();
+    this.updateFlashlightStatus();
+  }
+
+  updateFlashlightStatus(){
+    this.isOn = this.flashlight.isSwitchedOn();
+  }
 }
